856de2e4629424aa2e3f8822460969371a2abe3f
Jubaer87 <siamislam2036@gmail.com>
2023-09-19 19:25:58 +0600

    Update

--- spring-boot-crud-example/src/main/java/com/backend/crud/example/entity/Product.java
+++ spring-boot-crud-example/src/main/java/com/backend/crud/example/entity/Product.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 
 
 
-//This is an simple API based project where we can interact with postman by using RestController in controller package;
+//This is a simple API based project where we can interact with postman by using RestController in controller package;
 //Our database name is javaproject, we can see our tables in our working workbench(MySQL) 
 //This project is important to learn the basics about STS, how we can add APIs in our applications.
 
